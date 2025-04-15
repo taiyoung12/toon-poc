fa6412d5027d56eb1544306647c24741f76a9d7d
taiyoung <qq3222@naver.com>
2025-04-15 17:09:11 +0900

    first commit

---
+++ src/test/kotlin/com/comics/lezhin/toon/poc/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.comics.lezhin.toon.poc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
