b55f556c69ddd08a25e3f90fcafe5f38cb1e97e3
taiyoung <qq3222@naver.com>
2025-04-16 16:04:29 +0900

    Feature: UserEntity 작성

--- src/main/kotlin/com/comics/lezhin/toon/poc/entity/UserEntity.kt
+++ src/main/kotlin/com/comics/lezhin/toon/poc/entity/UserEntity.kt
@@ -1,4 +1,24 @@
 package com.comics.lezhin.toon.poc.entity
 
-class UserEntity {
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Table
+import org.hibernate.annotations.SQLDelete
+import org.hibernate.annotations.SQLRestriction
+
+@Entity
+@Table(name = "users")
+@SQLDelete(sql = "UPDATE users SET deleted_at = NOW() WHERE id = ?")
+@SQLRestriction("deleted_at IS NULL")
+class UserEntity(
+    name: String,
+    isAdult: Boolean,
+) : EditableBaseEntity() {
+    @Column(nullable = false)
+    var name: String = name
+        protected set
+
+    @Column(name = "is_adult", nullable = false)
+    var isAdult: Boolean = isAdult
+        protected set
 }
