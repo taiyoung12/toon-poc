4c19461aea6ef38aa09c2a180296fa72c1d9fd42
taiyoung <qq3222@naver.com>
2025-04-16 11:00:21 +0900

    Feature: PropertiesConfiguration 작성

---
+++ src/main/kotlin/com/comics/lezhin/toon/poc/config/PropertiesConfiguration.kt
@@ -0,0 +1,8 @@
+package com.comics.lezhin.toon.poc.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ConfigurationProperties("comics.lezhin.toon.poc")
+class PropertiesConfiguration
