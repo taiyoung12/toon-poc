adc4049260d276fcd755d2786c48bd09ac57aad1
taiyoung <qq3222@naver.com>
2025-04-16 16:02:40 +0900

    Chore: javax all open -> jakarta 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,9 +20,15 @@ repositories {
 }
 
 allOpen {
-    annotation("javax.persistence.Entity")
-    annotation("javax.persistence.MappedSuperclass")
-    annotation("javax.persistence.Embeddable")
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
+
+noArg {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
 }
 
 dependencies {
