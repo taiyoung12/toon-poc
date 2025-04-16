3167ce078f50fcb96971430ec13e591aa101f771
taiyoung <qq3222@naver.com>
2025-04-16 10:28:21 +0900

    Feature: Base Code 작성

---
+++ src/main/kotlin/com/comics/lezhin/toon/poc/common/Code.kt
@@ -0,0 +1,9 @@
+package com.comics.lezhin.toon.poc.common
+
+interface Code {
+    fun getCode(): String
+
+    fun getMessage(): String
+
+    fun getHttpStatus(): String
+}
---
+++ src/main/kotlin/com/comics/lezhin/toon/poc/common/CommonCode.kt
@@ -0,0 +1,25 @@
+package com.comics.lezhin.toon.poc.common
+
+import org.springframework.http.HttpStatus
+
+enum class CommonCode(
+    private val code: String,
+    private val message: String,
+    private val httpStatus: HttpStatus,
+) : Code {
+    SUCCESS("CM200", "성공", HttpStatus.OK),
+
+    INVALID_INPUT("CM400", "잘못된 입력입니다", HttpStatus.BAD_REQUEST),
+    VALIDATION_ERROR("CM401", "유효성 검사에 실패했습니다", HttpStatus.BAD_REQUEST),
+    UNAUTHORIZED("CM401", "인증이 필요합니다", HttpStatus.UNAUTHORIZED),
+
+    INTERNAL_ERROR("CM500", "서버 내부 오류가 발생했습니다", HttpStatus.INTERNAL_SERVER_ERROR),
+    RESOURCE_NOT_FOUND("CM404", "존재하지 않는 API END-POINT 입니다", HttpStatus.NOT_FOUND),
+    ;
+
+    override fun getCode(): String = code
+
+    override fun getMessage(): String = message
+
+    override fun getHttpStatus(): String = httpStatus.name
+}
