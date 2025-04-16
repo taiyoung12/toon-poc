ee3535b3315fb2b0a531b09a0c30d4682531d1d2
taiyoung <qq3222@naver.com>
2025-04-16 11:07:53 +0900

    Feature: HealthChecker 작성

--- src/main/kotlin/com/comics/lezhin/toon/poc/controller/health/PingController.kt
+++ src/main/kotlin/com/comics/lezhin/toon/poc/controller/health/PingController.kt
@@ -1,4 +1,13 @@
 package com.comics.lezhin.toon.poc.controller.health
 
+import com.comics.lezhin.toon.poc.common.response.Response
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("\${api.prefix}")
 class PingController {
+    @GetMapping("/ping")
+    fun pong(): Response<String> = Response.success("pong")
 }
