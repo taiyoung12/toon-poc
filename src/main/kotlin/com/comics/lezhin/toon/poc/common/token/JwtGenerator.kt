c9298fe8e33182fbcac16df7fcbd9c3e46759298
taiyoung <qq3222@naver.com>
2025-04-16 14:41:22 +0900

    Feature: JwtGenerator 작성

--- src/main/kotlin/com/comics/lezhin/toon/poc/common/token/JwtGenerator.kt
+++ src/main/kotlin/com/comics/lezhin/toon/poc/common/token/JwtGenerator.kt
@@ -1,2 +1,26 @@
-package com.comics.lezhin.toon.poc.common.token 
+package com.comics.lezhin.toon.poc.common.token
 
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.security.Keys
+import org.springframework.stereotype.Component
+import java.security.Key
+import java.util.Date
+
+@Component
+class JwtGenerator(
+    private val jwtProperties: JwtProperties,
+) {
+    private val secretKey: Key = Keys.hmacShaKeyFor(jwtProperties.secretKey.toByteArray())
+
+    fun generateAccessToken(subject: String): String {
+        val now = System.currentTimeMillis()
+        return Jwts
+            .builder()
+            .setSubject(subject)
+            .setIssuedAt(Date(now))
+            .setExpiration(Date(now + jwtProperties.accessTokenExpiration))
+            .signWith(secretKey, SignatureAlgorithm.HS256)
+            .compact()
+    }
+}
