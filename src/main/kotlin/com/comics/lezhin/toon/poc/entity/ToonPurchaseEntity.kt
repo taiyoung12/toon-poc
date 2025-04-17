ef92266ce3c25b9b369b19a20dcb823f49334086
taiyoung <qq3222@naver.com>
2025-04-17 15:19:03 +0900

    Feature: ToonPurchaseEntity 작성

---
+++ src/main/kotlin/com/comics/lezhin/toon/poc/entity/ToonPurchaseEntity.kt
@@ -0,0 +1,31 @@
+package com.comics.lezhin.toon.poc.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Table
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "toon_purchase")
+class ToonPurchaseEntity(
+    userId: Long,
+    toonId: Long,
+    purchasedAt: LocalDateTime,
+    price: Int,
+) : BaseEntity() {
+    @Column(name = "user_id", nullable = false)
+    var userId: Long = userId
+        protected set
+
+    @Column(name = "toon_id", nullable = false)
+    var toonId: Long = toonId
+        protected set
+
+    @Column(name = "purchased_at", nullable = false)
+    var purchasedAt: LocalDateTime = purchasedAt
+        protected set
+
+    @Column(name = "price", nullable = false)
+    var price: Int = price
+        protected set
+}
