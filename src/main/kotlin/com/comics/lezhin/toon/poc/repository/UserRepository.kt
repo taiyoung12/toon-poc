54bd47d5d2b4386e9cdd7799ee5f87689b5297e3
taiyoung <qq3222@naver.com>
2025-04-20 10:51:13 +0900

    Feature: Find User Query 작성

---
+++ src/main/kotlin/com/comics/lezhin/toon/poc/repository/UserRepository.kt
@@ -0,0 +1,8 @@
+package com.comics.lezhin.toon.poc.repository
+
+import com.comics.lezhin.toon.poc.entity.UserEntity
+import org.springframework.data.repository.Repository
+
+interface UserRepository : Repository<UserEntity, Long> {
+    fun findByEmail(email: String): UserEntity?
+}
