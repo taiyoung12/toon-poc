88ffc42dd1e0c2d2a93215561b53b8691057e979
taiyoung <qq3222@naver.com>
2025-04-20 11:15:50 +0900

    Feature: PasswordMatcher 작성

--- src/main/kotlin/com/comics/lezhin/toon/poc/common/utils/PasswordMatcher.kt
+++ src/main/kotlin/com/comics/lezhin/toon/poc/common/utils/PasswordMatcher.kt
@@ -1,4 +1,12 @@
 package com.comics.lezhin.toon.poc.common.utils
 
+import org.springframework.security.crypto.bcrypt.BCrypt
+import org.springframework.stereotype.Component
+
+@Component
 class PasswordMatcher {
+    fun matches(
+        rawPassword: String?,
+        encodedPassword: String?,
+    ): Boolean = BCrypt.checkpw(rawPassword, encodedPassword)
 }
