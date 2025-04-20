9f1814e8d4ba557b7ba6f955c27f07b12d5fd3ea
taiyoung <qq3222@naver.com>
2025-04-20 18:26:41 +0900

    Feature: UserCoinRepository find user for update query 작성

--- src/main/kotlin/com/comics/lezhin/toon/poc/repository/UserCoinRepository.kt
+++ src/main/kotlin/com/comics/lezhin/toon/poc/repository/UserCoinRepository.kt
@@ -1,8 +1,15 @@
 package com.comics.lezhin.toon.poc.repository
 
 import com.comics.lezhin.toon.poc.entity.UserCoinEntity
+import jakarta.persistence.LockModeType
+import org.springframework.data.jpa.repository.Lock
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.Repository
 
 interface UserCoinRepository : Repository<UserCoinEntity, Long> {
     fun findByUserId(userId: Long): UserCoinEntity?
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT uc FROM UserCoinEntity uc WHERE uc.userId = :userId")
+    fun findByUserIdForUpdate(userId: Long): UserCoinEntity?
 }
