67dd2a0aebcd0f3f33c12361a2b195b853c425e4
taiyoung <qq3222@naver.com>
2025-04-21 00:01:45 +0900

    Feature: ToonReader read top 10 service layer 작성

--- src/main/kotlin/com/comics/lezhin/toon/poc/service/toon/ToonReader.kt
+++ src/main/kotlin/com/comics/lezhin/toon/poc/service/toon/ToonReader.kt
@@ -3,6 +3,7 @@ package com.comics.lezhin.toon.poc.service.toon
 import com.comics.lezhin.toon.poc.app.exception.BaseException
 import com.comics.lezhin.toon.poc.common.code.ToonCode
 import com.comics.lezhin.toon.poc.entity.ToonEntity
+import com.comics.lezhin.toon.poc.inmemory.ViewRepository
 import com.comics.lezhin.toon.poc.repository.ToonRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -10,10 +11,15 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class ToonReader(
     private val toonRepository: ToonRepository,
+    private val viewRepository: ViewRepository,
 ) {
     @Transactional(readOnly = true)
     fun getToonBy(id: Long): ToonEntity {
         val toonEntity = toonRepository.findById(id = id) ?: throw BaseException(ToonCode.NOT_FOUND_TOON_BY_ID)
         return toonEntity
     }
+
+    fun findPopularAdultToonTop10() = viewRepository.getAdultTop10()
+
+    fun findPopularGeneralToonTop10() = viewRepository.getGeneralTop10()
 }
