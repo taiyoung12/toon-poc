6f535622e2480e63f2ddf0de02620083985c7e5d
taiyoung <qq3222@naver.com>
2025-04-20 13:02:58 +0900

    Docs: login api docs

--- src/main/kotlin/com/comics/lezhin/toon/poc/controller/auth/AuthController.kt
+++ src/main/kotlin/com/comics/lezhin/toon/poc/controller/auth/AuthController.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("\${api.prefix}/auth")
+@RequestMapping("/api/v1/auth")
 class AuthController(
     private val authApplication: AuthApplication,
 ) {
