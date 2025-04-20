52ff8ee24edd3e172d21edac1d0a3deaa391487d
taiyoung <qq3222@naver.com>
2025-04-20 16:19:07 +0900

    Feature: ToonController ReadToonViewHistory 작성

--- src/main/kotlin/com/comics/lezhin/toon/poc/common/code/ToonViewCode.kt
+++ src/main/kotlin/com/comics/lezhin/toon/poc/common/code/ToonViewCode.kt
@@ -1,4 +1,18 @@
 package com.comics.lezhin.toon.poc.common.code
 
-enum class ToonViewCode {
+import org.springframework.http.HttpStatus
+
+enum class ToonViewCode(
+    private val code: String,
+    private val message: String,
+    private val httpStatus: HttpStatus,
+) : Code {
+    SUCCESS("TV200", "성공", HttpStatus.OK),
+    ;
+
+    override fun getCode(): String = code
+
+    override fun getMessage(): String = message
+
+    override fun getHttpStatus(): String = httpStatus.name
 }
--- src/main/kotlin/com/comics/lezhin/toon/poc/controller/toon/ToonController.kt
+++ src/main/kotlin/com/comics/lezhin/toon/poc/controller/toon/ToonController.kt
@@ -1,4 +1,24 @@
 package com.comics.lezhin.toon.poc.controller.toon
 
-class ToonController {
+import com.comics.lezhin.toon.poc.app.response.Response
+import com.comics.lezhin.toon.poc.application.toon.ToonViewApplication
+import com.comics.lezhin.toon.poc.common.code.ToonViewCode
+import com.comics.lezhin.toon.poc.controller.response.ReadToonViewHistoryResponse
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/toon")
+class ToonController(
+    private val toonViewApplication: ToonViewApplication,
+) {
+    @GetMapping("/{toonId}/viewed")
+    fun readToonViewHistory(
+        @PathVariable("toonId") toonId: Long,
+    ): Response<ReadToonViewHistoryResponse?> {
+        val response = toonViewApplication.readToonViewHistory(id = toonId)
+        return Response.success(ToonViewCode.SUCCESS, response)
+    }
 }
